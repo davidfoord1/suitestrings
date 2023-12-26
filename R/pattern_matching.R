02f374376c5dc138ddcb352206d5649ffc32311c
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2023-12-26 20:14:46 +0000

    Add str_detect_match() documentation

--- R/pattern_matching.R
+++ R/pattern_matching.R
@@ -1,3 +1,18 @@
+#' Detect the presence of a pattern in a string
+#'
+#' @description
+#' These functions search for patterns in strings
+#'
+#' @param string A character vector of strings to search in.
+#' @param pattern The pattern to look for.
+#'
+#' @return A logical vector indicating the presence of each pattern in a string.
+#' @export
+#' @rdname str_detect
+#'
+#' @examples
+#' str_detect_match(c("apple", "banana", "cherry"), "a")
+#' #> [1]  TRUE  TRUE FALSE
 str_detect_match <- function(string, pattern) {
   grepl(pattern, string, perl = TRUE)
 }
