770c9e5f084e0bee68ca2a1d4ec0901ed69e1b33
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2023-12-27 08:15:22 +0000

    Add str_which() test

--- tests/testthat/test-pattern_matching.R
+++ tests/testthat/test-pattern_matching.R
@@ -1,6 +1,15 @@
+# str_detect ----------------------------------------------------
+
 test_that("str_detect_match() detects a pattern in a string", {
   string <- c("apple", "banana", "cherry")
   pattern <- "a"
 
   expect_equal(str_detect_match(string, pattern), c(TRUE, TRUE, FALSE))
 })
+
+# str_which -----------------------------------------------------
+
+test_that("str_which() identifies the matching indices in a character vector", {
+  expect_equal(str_which(letters, "[aeiou]"), c(1, 5, 9, 15, 21))
+})
+
