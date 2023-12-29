b6b9a2b93549d82d9462e4c275ebbadc55a8b1d8
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2023-12-29 14:24:36 +0000

    Add chr_split_all() tests

--- tests/testthat/test-vector-pattern-matching.R
+++ tests/testthat/test-vector-pattern-matching.R
@@ -10,3 +10,27 @@ test_that("chr_which() identifies the matching indices in a character vector", {
 test_that("chr_count_matches() counts the number of matching indices in a vector", {
   expect_equal(chr_count_matches(letters, "[aeiou]"), 5L)
 })
+
+# chr_split -----------------------------------------------------
+
+test_that("chr_split_all splits and concatenates all strings correctly", {
+  expect_equal(chr_split_all("a,b,c", ","), c("a", "b", "c"))
+  expect_equal(chr_split_all("hello world", " "), c("hello", "world"))
+})
+
+test_that("chr_split_all handles no matches", {
+  expect_equal(chr_split_all("hello", ","), "hello")
+})
+
+test_that("chr_split_all handles empty pattern", {
+  expect_equal(chr_split_all(c("abc", "def"), ""), c("a", "b", "c", "d", "e", "f"))
+})
+
+test_that("chr_split_all handles empty string", {
+  expect_equal(chr_split_all(c("a,b", ""), ","), c("a", "b"))
+})
+
+test_that("chr_split_all handles multiple strings", {
+  expect_equal(chr_split_all(c("a,b,c", "d,e"), ","), c("a", "b", "c", "d", "e"))
+})
+
