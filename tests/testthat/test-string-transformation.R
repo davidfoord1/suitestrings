3db585f1b7074a5cdb77b66ef25c33d4edbff085
davidfoord1 <david.foord1@nhs.net>
2023-12-30 19:04:07 +0000

    Add str_to_lower_case() tests

--- tests/testthat/test-string-transformation.R
+++ tests/testthat/test-string-transformation.R
@@ -7,3 +7,11 @@ test_that("str_to_upper_case converts to upper case correctly", {
   expect_equal(str_to_upper_case("123 ABC"), "123 ABC")
 })
 
+
+## str_to_lower_case -------------------------------------------
+
+test_that("str_to_lower_case converts to lower case correctly", {
+  expect_equal(str_to_lower_case("HELLO WORLD"), "hello world")
+  expect_equal(str_to_lower_case("Hello World"), "hello world")
+  expect_equal(str_to_lower_case("123 ABC"), "123 abc")
+})
