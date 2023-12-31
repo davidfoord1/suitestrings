0540fc082ce0a0c3fc83921647080f6654d88b26
davidfoord1 <david.foord1@nhs.net>
2023-12-31 10:44:15 +0000

    Initial argument-handler commit
    Add check_strings_arg() and check_pattern_arg() to check for and coerce
    to character. Only the first element of argument 'pattern' will be
    taken. A warning message will be given for any changes applied to arguments.

---
+++ R/argument-handler.R
@@ -0,0 +1,33 @@
+check_strings_arg <- function(replacement, function_name) {
+  if (!is.character(strings)) {
+    warning(paste("In", function_name, "(): \n",
+                  "`strings` is not a character vector.",
+                  "Coercing with `as.character()`"))
+
+    strings <- as.character(patterns)
+  }
+
+  strings
+}
+
+check_pattern_arg <- function(pattern, function_name) {
+  if (!is.character(pattern)) {
+    warning(paste("In", function_name, "(): \n",
+                   "`pattern` is not a character vector.",
+                   "Coercing with `as.character()`"))
+
+    pattern <- as.character(pattern)
+  }
+
+  if (length(pattern) > 1) {
+    warning(paste("In ", function_name, "():\n",
+                   "Argument `pattern` is of `length()` > 1;",
+                   "only the first element will be used. ",
+                   "To match multiple patterns,",
+                   "consider `pattern_any_string()` or `mapply()`."))
+
+    pattern <- pattern[[1]]
+  }
+
+  pattern
+}
