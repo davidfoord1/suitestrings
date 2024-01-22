9bfbf7bf77574e0f835b6512295f8a5d09a64251
davidfoord1 <david.foord1@nhs.net>
2024-01-22 17:50:01 +0000

    Add str_squish() tests

--- tests/testthat/test-string-transformation.R
+++ tests/testthat/test-string-transformation.R
@@ -59,4 +59,34 @@ test_that("str_trim does nothing to a string without leading/trailing whitespace
 
 # str_squish --------------------------------------------------------------
 
+test_that("str_squish removes whitespace at the front", {
+  expect_equal(str_squish("    Hello"), "Hello")
+  expect_equal(str_squish(c("  Hello", "   world")), c("Hello", "world"))
+})
 
+test_that("str_squish removes whitespace at the end", {
+  expect_equal(str_squish("Hello   "), "Hello")
+  expect_equal(str_squish(c("Hello   ", "world    ")), c("Hello", "world"))
+})
+
+test_that("str_squish removes additional whitespace in the middle", {
+  expect_equal(str_squish("Hello     World"), "Hello World")
+  expect_equal(str_squish(c("Hello   World", "Hello    friend")),
+                          c("Hello World", "Hello friend"))
+})
+
+test_that("str_squish removes extra whitespace", {
+  expect_equal(str_squish(" Hello   World "), "Hello World")
+  expect_equal(str_squish(c(" Hello  World  ", "  Hello   friend   ")),
+               c("Hello World", "Hello friend"))
+})
+
+test_that("str_squish empties a string with only whitespace", {
+  expect_equal(str_squish("   "), "")
+  expect_equal(str_squish("                                      "), "")
+})
+
+test_that("str_squish does nothing to a string without extra whitespace", {
+  expect_equal(str_squish("Hello"), "Hello")
+  expect_equal(str_squish(c("Hello", "world")), c("Hello", "world"))
+})
