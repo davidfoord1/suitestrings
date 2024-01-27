e0e61370f1c0a1bd27528304f81ea5d5a1a61339
David Foord <140633967+davidfoord1@users.noreply.github.com>
2024-01-27 14:12:33 +0000

    Add whitespace functions (#12)
    Add whitespace functions to string-transformation.R, their tests to test-string-transformation.R and their documentation /man and the README.
    
    - `str_trim()`
    - str_squish()`
    - str_indent()`
    - `str_pad()`
    - `str_wrap()`

--- R/string-transformation.R
+++ R/string-transformation.R
@@ -63,3 +63,232 @@ str_to_snake_case <- function(strings) {
 
   tolower(strings)
 }
+
+#' Remove trailing and leading whitespace
+#'
+#' @param strings
+#' A character vector, where each element of the vector is a character string.
+#'
+#' @return
+#' The same character vector, but with whitespace removed from the start and end
+#' of each string.
+#'
+#' @rdname str_trim
+#' @export
+#'
+#' @examples
+#' print(str_trim("    Remove these extra spaces    "))
+#' #> [1] "Remove these extra spaces"
+#' print(str_trim("Remove trailing new line character \n"))
+#' #> [1] "Remove trailing new line character"
+#' print(str_trim("But leave space     in   the      middle"))
+#' #> [1] "But leave space     in   the      middle"
+#'
+#' print(str_squish(" Don't leave   space    in  the   middle   here  "))
+#' #> [1] "Don't leave space in the middle here"
+str_trim <- function(strings) {
+  strings <- gsub("^[[:space:]]+|[[:space:]]+$", "", strings)
+  strings
+}
+
+#' @export
+#' @rdname str_trim
+str_squish <- function(strings) {
+  strings <- gsub("[[:space:]]+", " ", str_trim(strings))
+  strings
+}
+
+#' Add characters to the ends of strings
+#'
+#' @param strings
+#' A character vector, where each element of the vector is a character string.
+#' @param times
+#' The number of times the `indent` should be repeated, on each side specified.
+#' @param indent
+#' The characters to be used to indent.
+#' @param side
+#' Which side of the string should the indent be applied, one of
+#' "left", "right" or "both". The left side is the default.
+#'
+#' @return A character vector of the same length as `strings`, with each
+#' element indented as specified by the `times`, `indent`, and `side` arguments.
+#' @export
+#'
+#' @seealso
+#' [str_concat()] and [str_glue()] for combining strings together.
+#'
+#' Base [paste0()] and [strrep()] used by this function.
+#'
+#' @examples
+#' str_indent(c("Hello", "World"), 3)
+#' #> [1] "   Hello" "   World"
+#' str_indent(c("Hello", "World"), 3, ".")
+#' #> [1] "...Hello" "...World"
+#' str_indent(c("Hello", "World"), 3, "-", "both")
+#' #> [1] "---Hello---" "---World---"
+#'
+#' # Get extra with it
+#' "wow" |>
+#'   str_indent(side = "both") |>
+#'   str_indent(3, indent = "->>") |>
+#'   str_indent(3,  side = "right", indent = "<<-") |>
+#'   str_indent(2, side = "both", indent = "-||-") |>
+#'   str_indent(2, indent = "[") |>
+#'   str_indent(2, side = "right", indent = "]")
+#' #> [1] "[[-||--||-->>->>->> wow <<-<<-<<--||--||-]]"
+str_indent <- function(strings,
+                       times = 1,
+                       side = c("left", "right", "both"),
+                       indent = " ") {
+  side = match.arg(side)
+
+  left_indent <- right_indent <- 0
+
+  if (side == "left" || side == "both") left_indent <- times
+  if (side == "right" || side == "both") right_indent <- times
+
+  paste0(strrep(indent, left_indent), strings, strrep(indent, right_indent))
+}
+
+#' Pad a string to a minimum length
+#'
+#' @param strings
+#' A character vector, where each element of the vector is a character string.
+#' @param min_length
+#' The minimum number of characters in the padded string. The default is one more
+#' character than the longest string in `strings`. Strings already longer than
+#' `min_length` will be unchanged.
+#' @param side
+#' Which side of the string should the indent be applied, one of
+#' "left", "right" or "both". The left side is the default. When "both", half
+#' the required padding length with be added to each side, with `prefer_side`
+#' controlling where odd additions go.
+#' @param pad
+#' A single character used to pad the space
+#' @param prefer_side
+#' If `side` is "both", which side should get one extra pad when `min_length`
+#' is odd? Default to the right side.
+#'
+#' @return
+#' A character vector equal in length to `strings`, with each string having a
+#' [str_length()] of `min_length`. Strings already longer than `min_length`
+#' will be unchanged.
+#'
+#' @export
+#'
+#' @seealso
+#' [str_indent()] for adding a specific number of characters.
+#'
+#' [str_concat()] and [str_glue()] for combining strings together.
+#'
+#' Base [paste0()] and [strrep()] used by this function.
+#'
+#' @examples
+#' str_pad(c("Hello", "World"))
+#' #> [1] " Hello" " World"
+#'
+#' str_pad(c("Hello", "World"), 10)
+#' #> [1] "     Hello" "     World"
+#'
+#' str_pad(c("Hello", "World"),
+#'         10,
+#'         side = "both")
+#' #> [1] "  Hello   " "  World   "
+#'
+#' str_pad(c("Hello", "World"),
+#'         10,
+#'         side = "both",
+#'         pad = ".")
+#' #> [1] "..Hello..." "..World..."
+#'
+#' str_pad(c("Hello", "World"),
+#'         10,
+#'         side = "both",
+#'         pad = ".",
+#'         prefer_side = "left")
+#' #> [1] "...Hello.." "...World.."
+str_pad <- function(strings,
+                    min_length = max(nchar(strings)) + 1,
+                    side = c("left", "right", "both"),
+                    pad = " ",
+                    prefer_side = c("right", "left")
+                    ) {
+  stopifnot(nchar(pad) == 1)
+  stopifnot(length(pad) == 1)
+
+  side <- match.arg(side)
+  prefer_side <- match.arg(prefer_side)
+
+  pad_length <- min_length - nchar(strings)
+
+  left_pad <- right_pad <- 0
+
+  if (side == "both") {
+    side_length <- pad_length/2
+    if (prefer_side == "right") {
+      left_pad <- floor(side_length)
+      right_pad <- ceiling(side_length)
+    } else {
+      right_pad <- floor(side_length)
+      left_pad <- ceiling(side_length)
+    }
+  }
+
+  if (side == "left") {
+    left_pad <- pad_length
+  }
+
+  if (side == "right") {
+    right_pad <- pad_length
+  }
+
+  paste0(strrep(pad, left_pad), strings, strrep(pad, right_pad))
+}
+
+#' Wrap character strings to format as paragraphs
+#'
+#' @description
+#' Each character string in the input is first split into paragraphs
+#' (or lines containing whitespace only).
+#' The paragraphs are then formatted by breaking lines at word boundaries.
+#' The target columns for wrapping lines and the indentation of the first
+#' and all subsequent lines of a paragraph can be controlled independently.
+#'
+#' @param strings
+#' #' A character vector, where each element of the vector is a character string.
+#' @param min_width
+#' A positive integer giving the minimum line width for for wrapping lines in the output.
+#' @param indent
+#' A non-negative integer giving the indentation of the first line in a paragraph.
+#' @param exdent
+#' A non-negative integer specifying the indentation of subsequent lines in paragraphs.
+#' @param initial
+#' A character string to be used as a prefix for the first line in each
+#' paragraph.
+#' @param prefix
+#' A character string to be used as a prefix for subsequent lines in each
+#' paragraph.
+#'
+#' @return
+#' A list of a character vector for each input string in `strings`.
+#' The new strings in each character vector are each a single line of the
+#' the string formatted by paragraph.
+#' @export
+#'
+#' @examples
+#' text <- paste(readLines(file.path(R.home("doc"), "THANKS")), collapse = "\n")
+#'
+#' writeLines(str_wrap(text, min_width = 60))
+#' writeLines(str_wrap(text, min_width = 60, indent = 5))
+#' writeLines(str_wrap(text, min_width = 60, exdent = 5))
+#' writeLines(str_wrap(text, min_width = 60, prefix = "> "))
+
+str_wrap <- function(strings,
+                     min_width = 80,
+                     indent = 0,
+                     exdent = 0,
+                     prefix = "",
+                     initial = prefix
+) {
+  strwrap(strings, min_width, indent, exdent, prefix, initial = initial)
+}
--- tests/testthat/test-string-transformation.R
+++ tests/testthat/test-string-transformation.R
@@ -26,3 +26,147 @@ test_that("str_to_snake_case converts to snake case correctly", {
   expect_equal(str_to_snake_case("Hello-World 123"), "hello_world_123")
   expect_equal(str_to_snake_case("--Hello**World--"), "hello_world")
 })
+
+
+# str_ whitespace ---------------------------------------------------------
+
+# str_trim ----------------------------------------------------------------
+
+test_that("str_trim removes whitespace at the front", {
+  expect_equal(str_trim("    Hello"), "Hello")
+  expect_equal(str_trim(c("  Hello", "   world")), c("Hello", "world"))
+})
+
+test_that("str_trim removes whitespace at the end", {
+  expect_equal(str_trim("Hello   "), "Hello")
+  expect_equal(str_trim(c("Hello   ", "world    ")), c("Hello", "world"))
+})
+
+test_that("str_trim removes whitespace at both ends", {
+  expect_equal(str_trim("  Hello   "), "Hello")
+  expect_equal(str_trim(c("   Hello   ", "   world    ")), c("Hello", "world"))
+})
+
+test_that("str_trim empties a string with only whitespace", {
+  expect_equal(str_trim("   "), "")
+  expect_equal(str_trim("                                      "), "")
+})
+
+test_that("str_trim does nothing to a string without leading/trailing whitespace", {
+  expect_equal(str_trim("Hello"), "Hello")
+  expect_equal(str_trim(c("Hello", "world")), c("Hello", "world"))
+})
+
+# str_squish --------------------------------------------------------------
+
+test_that("str_squish removes whitespace at the front", {
+  expect_equal(str_squish("    Hello"), "Hello")
+  expect_equal(str_squish(c("  Hello", "   world")), c("Hello", "world"))
+})
+
+test_that("str_squish removes whitespace at the end", {
+  expect_equal(str_squish("Hello   "), "Hello")
+  expect_equal(str_squish(c("Hello   ", "world    ")), c("Hello", "world"))
+})
+
+test_that("str_squish removes additional whitespace in the middle", {
+  expect_equal(str_squish("Hello     World"), "Hello World")
+  expect_equal(str_squish(c("Hello   World", "Hello    friend")),
+                          c("Hello World", "Hello friend"))
+})
+
+test_that("str_squish removes extra whitespace", {
+  expect_equal(str_squish(" Hello   World "), "Hello World")
+  expect_equal(str_squish(c(" Hello  World  ", "  Hello   friend   ")),
+               c("Hello World", "Hello friend"))
+})
+
+test_that("str_squish empties a string with only whitespace", {
+  expect_equal(str_squish("   "), "")
+  expect_equal(str_squish("                                      "), "")
+})
+
+test_that("str_squish does nothing to a string without extra whitespace", {
+  expect_equal(str_squish("Hello"), "Hello")
+  expect_equal(str_squish(c("Hello", "world")), c("Hello", "world"))
+})
+
+# str_indent ---------------------------------------------------------------
+
+test_that("str_indent adds indent to the left", {
+  expect_equal(str_indent("hello", 3), "   hello")
+  expect_equal(str_indent(c("hello", "world"), 3),
+               c("   hello", "   world"))
+  expect_equal(str_indent("hello", 3, "left"), "   hello")
+  expect_equal(str_indent(c("hello", "world"), 3, "left"),
+               c("   hello", "   world"))
+})
+
+test_that("str_indent adds indent to the right", {
+  expect_equal(str_indent("hello", 3, "right"), "hello   ")
+  expect_equal(str_indent(c("hello", "world"), 3, "right"),
+               c("hello   ", "world   "))
+})
+
+test_that("str_indent adds indent to both sides", {
+  expect_equal(str_indent("hello", 3, "both"), "   hello   ")
+  expect_equal(str_indent(c("hello", "world"), 3, "both"),
+               c("   hello   ", "   world   "))
+})
+
+test_that("str_indent applies a custom indent character", {
+  expect_equal(str_indent("hello", 3, indent = "."), "...hello")
+  expect_equal(str_indent(c("hello", "world"), 3, indent = "."),
+               c("...hello", "...world"))
+})
+
+test_that("str_indent handles empty strings and vectors", {
+  expect_equal(str_indent("", 3), "   ")
+  expect_equal(str_indent(character(), 3), "   ")
+})
+
+# str_pad -----------------------------------------------------------------
+
+test_that("str_pad adds padding to the left", {
+  expect_equal(str_pad("hello"), " hello")
+  expect_equal(str_pad(c("hello", "world")), c(" hello", " world"))
+  expect_equal(str_pad("hello", side = "left"), " hello")
+  expect_equal(str_pad(c("hello", "world"), side = "left"),
+               c(" hello", " world"))
+})
+
+test_that("str_pad adds padding to the right", {
+  expect_equal(str_pad("hello", side = "right"), "hello ")
+  expect_equal(str_pad(c("hello", "world"), side = "right"),
+               c("hello ", "world "))
+})
+
+test_that("str_pad pads to a specified length", {
+  expect_equal(str_pad("hello", 10), "     hello")
+  expect_equal(str_pad(c("hello", "world"), 10), c("     hello", "     world"))
+})
+
+test_that("str_pad pads to both sides", {
+  expect_equal(str_pad("hello", 10, side = "both"), "  hello   ")
+  expect_equal(str_pad(c("hello", "world"), 10, side = "both"),
+               c("  hello   ", "  world   "))
+})
+
+test_that("str_pad prefers the left when specified", {
+  expect_equal(str_pad("hello", 10, side = "both", prefer_side = "left"),
+               "   hello  ")
+  expect_equal(str_pad(c("hello", "world"), 10, side = "both", prefer_side = "left"),
+               c("   hello  ", "   world  "))
+})
+
+test_that("str_pad takes a custom pad character", {
+  expect_equal(str_pad("hello", pad = "."), ".hello")
+  expect_equal(str_pad(c("hello", "world"), pad = "."),
+               c(".hello", ".world"))
+})
+
+test_that("str_pad only accepts one character for padding", {
+  expect_error(str_pad("hello", pad = ".."))
+  expect_error(str_pad("hello", pad = ""))
+  expect_error(str_pad("hello", pad = character(0)))
+})
